af689b6be7de05f925814ce697362987b62cee60
Crystal Broach <crysland@gmail.com>
2020-09-11 17:06:40 -0500

    while and do while loops php

---
+++ whileLoops.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Php While Loops</title>
+</head>
+<body>
+  
+  <?php
+    $index = 6;
+    do {
+      echo "$index <br>";
+      $index++;
+    } while($index <= 5);
+  ?>
+
+
+</body>
+</html>
