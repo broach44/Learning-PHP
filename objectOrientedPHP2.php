ec1bcb18a0eb3e613b4430f8bc99d363972746d6
Crystal Broach <crysland@gmail.com>
2020-09-11 19:17:31 -0500

    object functions

--- objectOrientedPHP2.php
+++ objectOrientedPHP2.php
@@ -18,8 +18,19 @@
         $this->major = $major;
         $this->gpa = $gpa;
       }
+
+      function hasHonors(){
+        if($this->gpa >= 3.5){
+          return "true";
+        }
+        return "false";
+      }
     }
 
+    $student1 = new Student("Jim", "Business", 2.8);
+    $student2 = new Student("Pam", "Art", 3.6);
+
+    echo $student2->hasHonors();
 
   ?>
 
